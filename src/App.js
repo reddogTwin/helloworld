ba14cb4d4af4145e081c0247e29b92f2d1a58c08
Steve Jamieson <stevejamieson@gmail.com>
2018-09-29 23:35:08 -0300

    combining two lists

--- src/App.js
+++ src/App.js
@@ -7,35 +7,52 @@ class App extends Component {
 
   render() {
     const numObj =
-    {
-      "employees":[
-              {id: 1, firstName:"John", lastName:"Doe"},
-              {id: 2, firstName :"Anna",  lastName:"Smith"},
-              {id: 3, firstName: "Peter", lastName:"Jones"}
-    ]
-  }
+[
+              {id: 1, firstName:"steve", lastName:"jamieson"},
+              {id: 2, firstName :"shena",  lastName:"walsh"},
+              {id: 3, firstName: "nora", lastName:"jamieson"}
+    ];
 
-  const numObj2 =
-  {
-    "details":[
+  const numObj2 = [
             {id: 1, thumnail:"http://steve"},
-            {id: 2, thumnail:"http://steve"},
-            {id: 3, thumnail:"http://steve"}
+            {id: 2, thumnail:"http://shena"},
+            {id: 3, thumnail:"http://nora"}
 
-  ]
+  ];
+
+  const numObj3 = [
+            {id: 1, name:"steve"},
+            {id: 2, name:"shena"},
+            {id: 3, name:"nora"}
+
+  ];
+
+let result = [{}];
+for(var i in numObj ){
+  for(var j in numObj2){
+    if(numObj[i].id === numObj2[j].id){
+      result.push(numObj[i].lastName);
+      result.push(numObj2[j].thumnail);
+    }
+  }
 }
+// const result = numObj.employees.map(word =>
+//   numObj2.details.map(word2 => word.id === word2.id));
 
-const result = numObj2.details.filter(word => word.id === 1);
+
+// const result = numObj2.details.filter(word =>
+//   numObj.employees.map(id => id.id === word.id));
 
 
     console.log(numObj);
     console.log(numObj2);
+    console.log(numObj3);
     console.log(result);
     // const numbers = [1, 2, 3, 4, 5];
     // const numbers2 = [6, 7, 8, 9, 10];
 
 
-    const listItems2 = numObj.employees.map((number) =>
+    const listItems2 = numObj.map((number) =>
       <li>{number.firstname}</li>
     );
 
